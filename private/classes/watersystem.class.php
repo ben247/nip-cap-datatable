3c9789f484ff6b2bbe681463c5e245772a661135
ben whalley <ben247@gmail.com>
2019-08-14 14:14:40 +0100

    created databaseObject class to abstract inheritable code for future classes

--- private/classes/watersystem.class.php
+++ private/classes/watersystem.class.php
@@ -1,145 +1,11 @@
 <?php
 
-class WaterSystem {
+class WaterSystem extends DatabaseObject {
 
-  // ----------START OF ACTIVE RECORD CODE----------
 // tell the class what database it should be using
 // static makes accessible without an instantiation
-  static protected $database;
+  static protected $table_name = 't01a_water_system';
   static protected $db_columns = ['id', 'system_name', 'area_council', 'island', 'province', 'latitude', 'longitude', 'elevation', 'resource_type', 'system_type', 'improved', 'functionality', 'number_users'];
-  public $errors = [];
-
-  static public function set_database($database) {
-    self::$database = $database;
-  }
-
-  static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM t01a_water_system";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM t01a_water_system ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
-  }
-
-  protected function validate() {
-    $this->errors = [];
-
-    if(is_blank($this->system_name)) {
-      $this->errors[] = "System Name cannot be blank.";
-    }
-    return $this->errors;
-  }
-
-  // create dynamic attribute list
-  public function create() {
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $sql = "INSERT INTO t01a_water_system (";
-    $sql .= join(', ', array_keys($attributes));
-    $sql .= ") VALUES ('";
-    $sql .= join("', '", array_values($attributes));
-    $sql .= "')";
-    $result = self::$database->query($sql);
-    if($result) {
-      $this->id = self::$database->insert_id;
-    }
-    return $result;
-  }
-
-  public function update() {
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $attribute_pairs = [];
-    foreach($attributes as $key => $value) {
-      $attribute_pairs[] = "{$key}='{$value}'";
-    }
-    $sql = "UPDATE t01a_water_system SET ";
-    $sql .= join(', ', $attribute_pairs);
-    $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-  }
-
-  public function merge_attributes($args=[]) {
-    foreach($args as $key => $value) {
-      if(property_exists($this, $key) && !is_null($value)) {
-        $this->$key = $value;
-      }
-    }
-  }
-
-  // properties which have database columns, excluding ID
-  public function attributes() {
-    $attributes = [];
-    foreach(self::$db_columns as $column) {
-      if($column == 'id') { continue; }
-      $attributes[$column] = $this->$column;
-    }
-    return $attributes;
-  }
-  // sanitize using escape_string()
-  protected function sanitized_attributes() {
-    $sanitized = [];
-    foreach($this->attributes() as $key => $value) {
-      $sanitized[$key] = self::$database->escape_string($value);
-    }
-    return $sanitized;
-  }
-
-  public function delete() {
-    $sql = "DELETE FROM t01a_water_system ";
-    $sql .= "WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-    // after deleting, the instance of the object will still exist
-    // this can be usful..
-    // echo $user->first_name . " was deleted. ";
-  }
-
-  // ----- END OF ACTIVE RECORD CODE ------
 
   public $system_name;
   public $id;
@@ -191,6 +57,15 @@ class WaterSystem {
 
   public function name() {
     return "{$this->system_name} {$this->area_council} {$this->island}";
+  }
+
+  protected function validate() {
+    $this->errors = [];
+
+    if(is_blank($this->system_name)) {
+      $this->errors[] = "System Name cannot be blank.";
+    }
+    return $this->errors;
   }
 
 }
