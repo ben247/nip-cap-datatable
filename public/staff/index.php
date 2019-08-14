58979f55390042ea330b52f245dc14ff5c3bfbb5
ben whalley <ben247@gmail.com>
2019-08-14 17:05:26 +0100

    added auth

--- public/staff/index.php
+++ public/staff/index.php
@@ -8,6 +8,7 @@
     <h2>Main Menu</h2>
     <ul>
       <li><a href="<?php echo url_for('/staff/watersystems/index.php'); ?>">Bicycles</a></li>
+      <li><a href="<?php echo url_for('/staff/admins/index.php'); ?>">Admins</a></li>
     </ul>
   </div>
 
